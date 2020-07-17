f8cc194b12288e080f7833f2afc96d2c406d31de
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-17 23:29:53 +0530

    ADDED Test for checking success for un-parking

--- src/test/java/com/parkingLot/ParkingLotTest.java
+++ src/test/java/com/parkingLot/ParkingLotTest.java
@@ -9,13 +9,13 @@ public class ParkingLotTest {
     ParkingLot parkingLot = new ParkingLot();
 
     @Test
-    public void givenACarObject_WhenCheckedForParking_IfAvailableReturnsTrue() throws ParkingLotException {
+    public void givenACarObject_WhenParking_IfAvailableReturnsTrue() throws ParkingLotException {
         Vehicle car = new Vehicle("RED", "MH22QW2991", "SUZUKI");
         Assert.assertTrue(parkingLot.park(car));
     }
 
     @Test
-    public void givenCarObject_WhenCheckedForParking_IfNotAvailableThrowsException() {
+    public void givenCarObject_WhenParking_IfNotAvailableThrowsException() {
         try {
             Vehicle car1 = new Vehicle("WHITE", "MH51QE8520", "TOYOTA");
             Vehicle car2 = new Vehicle("RED", "MH22QW2991", "SUZUKI");
@@ -24,5 +24,12 @@ public class ParkingLotTest {
         } catch (ParkingLotException e) {
             Assert.assertEquals(SPACE_NOT_AVAILABLE, e.type);
         }
+    }
+
+    @Test
+    public void givenParkedCarObject_WhenUnParked_IfSuccessReturnTrue() throws ParkingLotException {
+        Vehicle car1 = new Vehicle("WHITE", "MH51QE8520", "TOYOTA");
+        parkingLot.park(car1);
+        Assert.assertTrue(parkingLot.unPark(car1));
     }
 }
