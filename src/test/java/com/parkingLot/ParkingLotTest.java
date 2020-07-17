2399d172965175a37428202a37e03bbf023f0a14
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-17 23:19:13 +0530

    ADDED Test to throw custom exception when parking lot is full

--- src/test/java/com/parkingLot/ParkingLotTest.java
+++ src/test/java/com/parkingLot/ParkingLotTest.java
@@ -3,6 +3,8 @@ package com.parkingLot;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static com.parkingLot.ParkingLotException.ExceptionType.SPACE_NOT_AVAILABLE;
+
 public class ParkingLotTest {
     ParkingLot parkingLot = new ParkingLot();
 
@@ -10,5 +12,17 @@ public class ParkingLotTest {
     public void givenACarObject_WhenCheckedForParking_IfAvailableReturnsTrue() throws ParkingLotException {
         Vehicle car = new Vehicle("RED", "MH22QW2991", "SUZUKI");
         Assert.assertTrue(parkingLot.park(car));
+    }
+
+    @Test
+    public void givenCarObject_WhenCheckedForParking_IfNotAvailableThrowsException() {
+        try {
+            Vehicle car1 = new Vehicle("WHITE", "MH51QE8520", "TOYOTA");
+            Vehicle car2 = new Vehicle("RED", "MH22QW2991", "SUZUKI");
+            parkingLot.park(car1);
+            parkingLot.park(car2);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals(SPACE_NOT_AVAILABLE, e.type);
+        }
     }
 }
