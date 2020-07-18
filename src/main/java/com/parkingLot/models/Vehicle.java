de99535c02100cc8585e3df9ae1e1b22516f0f63
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-18 10:15:47 +0530

    REFACTORED Parking lot for multiple lots and created proper package structure

--- src/main/java/com/parkingLot/models/Vehicle.java
+++ src/main/java/com/parkingLot/models/Vehicle.java
@@ -1,11 +1,11 @@
-package com.parkingLot;
+package com.parkingLot.models;
 
 public class Vehicle {
     String color;
     String numberPlate;
     String make;
 
-    Vehicle(String color, String numberPlate, String make) {
+    public Vehicle(String color, String numberPlate, String make) {
         this.color = color;
         this.numberPlate = numberPlate;
         this.make = make;
